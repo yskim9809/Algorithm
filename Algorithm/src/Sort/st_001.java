300c00a0ecf4b55fe2129242ebd50a7db7df03c5
yskim9809 <dudtj9809@gmail.com>
2025-04-25 23:01:28 +0900

    20250425

--- Algorithm/src/Sort/st_001.java
+++ Algorithm/src/Sort/st_001.java
@@ -8,6 +8,7 @@ public class st_001 {
 	 */
 	public static void main(String[] args) {
 		int[] arr = { 9, 1, 6, 8, 2 };
+		
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr.length - 1; j++) {
 				if (arr[j] > arr[j + 1]) {
