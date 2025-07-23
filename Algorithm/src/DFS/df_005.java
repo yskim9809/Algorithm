73b5c91834d44f78864957e957c1b8fcb1587bb3
yskim9809 <dudtj9809@gmail.com>
2025-07-24 00:51:44 +0900

    20250724

---
+++ Algorithm/src/DFS/df_005.java
@@ -0,0 +1,52 @@
+package DFS;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class df_005 {
+
+	public static void main(String[] args) {
+		int[][] maps = {
+			    {0, 1, 1, 0},
+			    {1, 1, 0, 0},
+			    {0, 0, 1, 1},
+			    {0, 1, 1, 0}
+			};
+			int n = maps.length;
+			int m = maps[0].length;
+			boolean[][] visited = new boolean[n][m];
+			int[] dx = { -1, 1, 0, 0 };
+			int[] dy = { 0, 0, -1, 1 }; // 위, 아래, 왼, 오
+			ArrayList<Integer> arr = new ArrayList<>();
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					if (maps[i][j] == 1 && !visited[i][j]) {
+						
+						int count = dfs(i, j, maps, visited, n, m, dx, dy);
+						arr.add(count);
+					}
+				}
+			}
+			Collections.sort(arr);
+			System.out.println(arr);
+
+		}
+
+		private static int dfs(int i, int j, int[][] maps, boolean[][] visited, int n, int m, int[] dx, int[] dy) {
+			visited[i][j] = true;
+			int count=1;
+			for (int d = 0; d < 4; d++) {
+				int nx = i + dx[d];
+				int ny = j + dy[d];
+				if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+					if(maps[nx][ny]==1&&!visited[nx][ny]) {
+						count+= dfs(nx, ny, maps, visited, n, m, dx, dy);
+					}
+				}
+			}
+			return count;
+
+		}
+
+}
