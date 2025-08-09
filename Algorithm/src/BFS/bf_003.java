89a39bfc398d48a68b3825b969e88da144d11c19
yskim9809 <dudtj9809@gmail.com>
2025-08-09 23:31:36 +0900

    20250809

---
+++ Algorithm/src/BFS/bf_003.java
@@ -0,0 +1,12 @@
+package BFS;
+
+public class bf_003 {
+
+	public static void main(String[] args) {
+		int N = 5;
+		int[][] transfers = { { 0, 1 }, { 1, 2 }, { 2, 4 }, { 0, 3 }, { 3, 4 } };
+		int T = 4;
+
+	}
+
+}
