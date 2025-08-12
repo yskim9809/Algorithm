29bacda94aa50261650dda5f2ca8e24890b59f1f
yskim9809 <dudtj9809@gmail.com>
2025-08-12 23:42:28 +0900

    20250812

--- Algorithm/src/BFS/bf_004.java
+++ Algorithm/src/BFS/bf_004.java
@@ -3,7 +3,15 @@ package BFS;
 public class bf_004 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] grid = { // (n-1,m-1) 로 가는 최단 이동 칸 수. 0으로만 이동 상하좌우가능
+	            {0, 0, 0},
+	            {1, 1, 0},
+	            {0, 0, 0}
+	        };
+		
+		int n = grid.length;
+		int m = grid[0].length;
+		boolean[][] visited = new boolean[n][m];
 
 	}
 
