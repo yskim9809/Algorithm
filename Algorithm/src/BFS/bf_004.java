568f7f6fa72f4e8f9f0ebacae37ea238df0d8fc2
yskim9809 <dudtj9809@gmail.com>
2025-08-14 00:24:43 +0900

    20250814

--- Algorithm/src/BFS/bf_004.java
+++ Algorithm/src/BFS/bf_004.java
@@ -1,5 +1,8 @@
 package BFS;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class bf_004 {
 
 	public static void main(String[] args) {
@@ -12,11 +15,56 @@ public class bf_004 {
 		int n = grid.length;
 		int m = grid[0].length;
 		boolean[][] visited = new boolean[n][m];
+		Queue<int[]> que = new LinkedList<>();
+
+		int[] dx = { 1, -1, 0, 0 };
+		int[] dy = { 0, 0, -1, 1 };
+
+		// 예외처리
+		if (n == 1 && m == 1) {
+			System.out.println(0);
+			return;
+		}
+		if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1) {
+			System.out.println(-1);
+			return;
+		}
 		
-		int[] dx = {1,-1,0,0};
-		int[] dy = {0,0,-1,1};
+		// 시작 세팅
+		que.add(new int[] { 0, 0, 0 }); // x,y,거리
+		visited[0][0] = true;
+
+		// BFD
+		while (!que.isEmpty()) {
+			int[] temp = que.poll();
+			int x = temp[0];
+			int y = temp[1];
+			int d = temp[2];
+
+			for (int di = 0; di < 4; di++) {
+				int nx = x + dx[di];
+				int ny = y + dy[di];
+
+				// 건너뛰기
+				if (nx < 0 || ny < 0 || nx >= n || ny >= m)
+					continue;
+				if (grid[nx][ny] == 1)
+					continue;
+				if (visited[nx][ny])
+					continue;
+
+				// 도착이면 즉시 종료
+				if (x == n - 1 && y == m - 1) {
+					System.out.println(d);
+					return;
+				}
+
+				visited[nx][ny] = true;
+				que.add(new int[] { nx, ny, d + 1 });
+			}
+		}
 		
-		 
+		System.out.println(-1);
 
 	}
 
